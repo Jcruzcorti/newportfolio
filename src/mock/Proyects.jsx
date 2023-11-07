6374298f8dd3b3c3b1c0802c009f393a17e3e5a6
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2023-11-07 15:21:31 -0300

    Update of projects in portfolio

--- src/mock/Proyects.jsx
+++ src/mock/Proyects.jsx
@@ -1,7 +1,16 @@
 export const proyects = [
 
+
+
     {
         id:'1',
+        tittle:'QUIEN JUEGA HOY',
+        description:"Football sports schedule exclusively, with the purpose of informing about today's and tomorrow's matches, with argentinian schedule. I used Next js and Material UI, consuming football API",
+        url:'https://quienjuegahoy.vercel.app/',
+        img:'/quienjuegahoy.png'      
+    },
+    {
+        id:'2',
         tittle:'RAIZ CARPINTERÍA DE DISEÑO',
         description:'Web application development for Raiz Carpintería de Diseño, using technologies such as Next.js and Material UI to build and style the application',
         url:'https://raizcarpinteria.vercel.app/',
@@ -9,7 +18,7 @@ export const proyects = [
     },
 
     {
-        id:'2',
+        id:'3',
         tittle:'PORTFOLIO IN SPANISH',
         description:'Development and re-version of Curriculum but in Spanish, transforming the previous website into a more powerful app with advanced technologies such as JavaScript, React.js and Next.js',
         url:'https://portfoliojcc.vercel.app/',
@@ -17,7 +26,7 @@ export const proyects = [
     },
     
     {
-        id:'3',
+        id:'4',
         tittle:'HARRY POTTER QUIZ',
         description:"Harry Potter themed quiz app using technologies such as JavaScript, React js, Next js and Firebase in order to save score and participant's name",
         url:'https://harrypotterquizgame.vercel.app/',
@@ -25,7 +34,7 @@ export const proyects = [
     },
     
     {
-        id:'4',
+        id:'5',
         tittle:'SCALONETA QUIZ',
         description:"Quiz app about Argentina National Team using technologies such as JavaScript, React js and Firebase to store participant's name and score",
         url:'https://scalonets.netlify.app/',
@@ -33,7 +42,7 @@ export const proyects = [
     },
     
     {
-        id:'5',
+        id:'6',
         tittle:'MM CHOCOLATES APP',
         description:'We created Mm Chocolates website reversion, transforming it into an e-commerce with technologies such as JavaScript, React.js and Firebase storing the purchase order made',
         url:'https://mmchocolates.netlify.app/',
@@ -41,7 +50,7 @@ export const proyects = [
     },
     
     {
-        id:'6',
+        id:'7',
         tittle:'NIKE STORE',
         description:'Development of Nike e-commerce app with advanced technologies such as JavaScript, React.js, Adobe Photoshop and Firebase in order to save the purchase order made by the user',
         url:'https://thenikestore.netlify.app/',
@@ -49,7 +58,7 @@ export const proyects = [
     },
     
     {
-        id:'7',
+        id:'8',
         tittle:'MM CHOCOLATES SITIO WEB',
         description:'Development and layout of a website for a chocolate company, with basic technologies such as HTML, CSS, Bootstrap and JavaScript using API Email.js to send orders',
         url:'https://jcruzcorti.github.io/MMchocolates/',
@@ -57,7 +66,7 @@ export const proyects = [
     },
     
     {
-        id:'8',
+        id:'9',
         tittle:'JUGADORES HISTÓRICOS',
         description:'Football Blog project about a selection of historical, current and future favorite soccer players, developed and laid out with basic technologies such as HTML, CSS, etc',
         url:'https://jcruzcorti.github.io/JH/',
@@ -65,7 +74,7 @@ export const proyects = [
     },
     
     {
-        id:'9',
+        id:'10',
         tittle:'CURRICULUM VITAE',
         description:'Project similar to the current portfolio, CV website with personal information and achievements, but developed and laid out with basic technologies such as HTML, CSS and Bootstrap',
         url:'https://jcruzcorti.github.io/CV/',
